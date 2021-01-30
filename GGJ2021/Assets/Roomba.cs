969b291a89175d9dd85c9c7204907b567f854d0f
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2021-01-31 08:43:53 +1100

    Adding Roomba Prefab

---
+++ GGJ2021/Assets/Roomba.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Roomba : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag("Cat"))
+        {
+            // Launch Game Over Event here
+            Debug.Log(this.gameObject.name + "has found the cat!");
+            
+        }
+    }
+}
