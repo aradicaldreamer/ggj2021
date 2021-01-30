f6b1bfffdcc8b55eb5d560895e2fe07112ed760b
Priyam-Swine <priyamj580@gmail.com>
2021-01-31 07:46:38 +1100

    Timer Update + GameOver Prototype

--- GGJ2021/Assets/Scripts/TImer.cs
+++ GGJ2021/Assets/Scripts/TImer.cs
@@ -2,22 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class TImer : MonoBehaviour
 {
    public GameObject textDisplay;
-   public int secondsLeft = 60;
+   public int secondsLeft = 0;
    public bool takingAway = false;
     // Start is called before the first frame update
     void Start()
     {
-        textDisplay.GetComponent<Text>().text = "00:" + secondsLeft;
+        textDisplay.GetComponent<TMP_Text>().text = "Time: " + secondsLeft+"s";
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (takingAway == false && secondsLeft > 0)
+        if (takingAway == false && secondsLeft > -1)
         {
           StartCoroutine(TimeTake());
         }
@@ -27,12 +28,12 @@ public class TImer : MonoBehaviour
     {
       takingAway = true;
       yield return new WaitForSeconds(1);
-      secondsLeft -=1;
+      secondsLeft +=1;
       if (secondsLeft < 10) {
-        textDisplay.GetComponent<Text>().text = "00:0"+ secondsLeft;
+        textDisplay.GetComponent<TMP_Text>().text = "Time: 0"+ secondsLeft+"s";
       }
       else{
-      textDisplay.GetComponent<Text>().text = "00:"+ secondsLeft;
+      textDisplay.GetComponent<TMP_Text>().text = "Time: "+ secondsLeft +"s";
     }
       takingAway = false;
     }
