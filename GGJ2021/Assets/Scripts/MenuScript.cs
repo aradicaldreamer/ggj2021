ac33ca9de479811167791fde5c32366d7b058c77
aradicaldreamer <31594961+aradicaldreamer@users.noreply.github.com>
2021-01-31 13:26:19 +1100

    Fixes to GameOver screen

--- GGJ2021/Assets/Scripts/MenuScript.cs
+++ GGJ2021/Assets/Scripts/MenuScript.cs
@@ -5,19 +5,39 @@ using UnityEngine.SceneManagement;
 
 public class MenuScript : MonoBehaviour
 {
+  
+  [FMODUnity.EventRef] public string musicTrackEvent;
+
+  public FMOD.Studio.EventInstance backgroundMusic;
+
+  void Start()
+  {
+        if (musicTrackEvent != null)
+        {
+          backgroundMusic = FMODUnity.RuntimeManager.CreateInstance(musicTrackEvent);
+          FMOD.Studio.EventDescription backgroundMusicEventDescription;
+          backgroundMusic.getDescription(out backgroundMusicEventDescription);
+
+          backgroundMusic.start();
+        }
+  }
+
   public void PlayGame ()
   {
+    backgroundMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     SceneManager.LoadScene("Game");
   }
 
   public void QuitGame()
   {
     Debug.Log("QUIT!");
+    backgroundMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     Application.Quit();
   }
 
   public void GOtoMain ()
   {
+    backgroundMusic.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
     SceneManager.LoadScene("Mainmenu");
   }
 }
